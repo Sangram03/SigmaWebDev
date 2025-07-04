a92b5c6553b676e150b7ad10702e713127f904e9
Sangram Das <sangramraju143@gmail.com>
2025-07-04 20:24:27 +0530

    here all function are added in javascript soon

---
+++ Day054/script.js
@@ -0,0 +1,22 @@
+alert("Hello World");
+
+// console.log("Hey I am console");
+console.log("Code is running...");
+
+
+var a = promt("enter Your number")
+var isTrue = conform("Are you sure you want to leave this page and blast your computer")
+if (isTrue) {
+    console.log("Computer is blasting");
+    
+}
+
+else {
+    console.log("Computer is not blasting");
+    
+}
+console.log("your number is "+a);
+
+document.title = "Hey I am "
+// document.body.style.backgroundColor="red"
+
---
+++ Day055/script.js
@@ -0,0 +1,59 @@
+console.log("Hey this is tutorial 55");
+
+
+var a = 5;
+var b = 6;
+var c = "Harry";
+
+var _a = "Shubham";
+
+// var is used as globally scoped 
+
+// var 55a = "Rohan";
+// not possible at first number
+
+console.log(a+b+8);
+console.log(typeof a, typeof b , typeof c);
+
+
+// const a1 = 6;
+// a1 = a1+1;  not allowed value cannot change
+
+
+{
+    let a = 66;
+    console.log(a);
+    // let  is block scope
+}
+
+console.log(a);
+
+
+let x ="Harry bhai";
+let y = 22;
+let z = 3.55;
+
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x , y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof q,typeof r);
+
+
+let o = {
+    // "key":"Value"
+    name:"Harry",
+    "job code": 5600,
+    // here due to space " " is added
+    "is_handsome":true,
+}
+
+
+console.log(o);
+o.salary = "100crores"
+console.log(o);
+
+
+
+

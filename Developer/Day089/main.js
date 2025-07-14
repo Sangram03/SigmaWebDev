7ecbc5e3ed7370bf53811a04649e23237038333b
Sangram Das <sangramraju143@gmail.com>
2025-07-14 21:51:38 +0530

    14/07/25

--- Developer/Day089/main.js
+++ Developer/Day089/main.js
@@ -2,7 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {
+    console.log("Heys its a get request");
+    
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    console.log("Heys its a post request");
+    
+  res.send('Hello World!')
+})
+
+app.get("/index",(req,res) => {
+  console.log("Hey its index");
+  res.sendFile('templates/index.html' ,{root:__dirname})
+  
+})
+
+app.get("/api",(req,res) => {
+res.json({a:1 , b:2, c:3 ,d:4 ,name: ["harry" , "jerry"] })
+  
+})
+
+
+app.put('/', (req, res) => {
+    console.log("Heys its a post request");
+    
   res.send('Hello World!')
 })
 

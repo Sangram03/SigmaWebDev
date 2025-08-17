f89c8865b3296b9e9dbd3302facc17c57e59de0a
Sangram Das <sangramraju143@gmail.com>
2025-08-17 13:49:58 +0530

    props completed

---
+++ Developer/Day106/first-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='hii'>
+      &copy; copyright www.codewithharry.com | All rights reserved
+    </div>
+  );
+}
+
+export default Footer;
